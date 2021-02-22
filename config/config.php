9544b19d4b8f6e6ffd01354211bfecb4fa72b94a
Charles <charles.stolive@gmail.com>
2021-02-22 11:21:13 +0100

    snappy dans le plugin

---
+++ config/config.php
@@ -0,0 +1,34 @@
+<?php return [
+    'packages' => [
+        'barryvdh' => [
+            'providers' => [
+                '\Barryvdh\Snappy\ServiceProvider',
+            ],
+
+            'aliases' => [
+                'SnappyPDF' => '\Barryvdh\Snappy\Facades\SnappyPdf',
+                'SnappyImage' => '\Barryvdh\Snappy\Facades\SnappyImage',
+            ],
+
+            'config_namespace' => 'snappy',
+
+            'config' => [
+                'pdf' => [
+                    'enabled' => true,
+                    'binary' => env("WKHTML_PDF_BINARY", '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"'),
+                    'timeout' => false,
+                    'options' => [],
+                    'env' => [],
+                ],
+
+                'image' => [
+                    'enabled' => true,
+                    'binary' => env("WKHTML_IMG_BINARY", '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"'),
+                    'timeout' => false,
+                    'options' => [],
+                    'env' => [],
+                ],
+            ],
+        ],
+    ],
+];
