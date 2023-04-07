9801555dfd3800313e4eb2c30e49f9e6aac910fb
Charles <charles@notilac.fr>
2023-04-07 16:03:25 +0200

    MAJ LANG

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,16 +1,17 @@
 <?php
 
 return [
-    "button" => [
-        "exemple_download" => "Télecharger Pdf",
-        "exemple_inline" => "Pdf en ligne",
-        "exemple_inline_show" => "Html en ligne"
+    'button' => [
+        'exemple_download' => 'Télecharger Pdf',
+        'exemple_inline' => 'Pdf en ligne',
+        'exemple_inline_show' => 'Html en ligne'
     ],
-    "errors" => [
-        "wakaPdfId" => "Il manque l'ID du PDF"
+    'errors' => [
+        'modelId' => 'Il manque l\'ID du modèle',
+        'wakaPdfId' => 'Il manque l\'ID du PDF'
     ],
-    "menu" => [
-        "wakapdfs" => "Pdfs",
-        "wakapdfs_description" => "Gestion des modèles pdfs"
+    'menu' => [
+        'wakapdfs' => 'Pdfs',
+        'wakapdfs_description' => 'Gestion des modèles pdfs'
     ]
 ];
--- lang/fr/layout.php
+++ lang/fr/layout.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    "add_css" => "CSS",
-    "create" => "Créer un template",
-    "footer_html" => "Contenu HTML Footer",
-    "form_name" => "Gestion du template",
-    "header_html" => "Contenu HTML header",
-    "layout_path" => "Chemin du template",
-    "layout_require_code" => "Code requis",
-    "name" => "Nom du template PDF",
-    "options" => "Option du pdf",
-    "preview_name" => "Voir le template",
-    "title" => "Gestion des templates",
-    "update" => "MAJ Pdf",
-    "use_footer" => "Utilser un footer",
-    "use_header" => "Utilser un header"
+    'add_css' => 'CSS',
+    'create' => 'Créer un template',
+    'footer_html' => 'Contenu HTML Footer',
+    'form_name' => 'Gestion du template',
+    'header_html' => 'Contenu HTML header',
+    'layout_path' => 'Chemin du template',
+    'layout_require_code' => 'Code requis',
+    'name' => 'Nom du template PDF',
+    'options' => 'Option du pdf',
+    'preview_name' => 'Voir le template',
+    'title' => 'Gestion des templates',
+    'update' => 'MAJ Pdf',
+    'use_footer' => 'Utilser un footer',
+    'use_header' => 'Utilser un header'
 ];
